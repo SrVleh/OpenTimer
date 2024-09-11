6601f0f1caedf6738fc7f761ac206b408625fb66
SrVleh <srvleh@gmail.com>
2024-09-11 11:17:56 +0200

    Basic routing and card ui component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,19 @@
 <script setup>
-
+import Card from "../shared/ui/Card.vue";
 </script>
 
 <template>
-  <h1>Hello Home</h1>
+  <section class="cards-container">
+    <Card link="#/" image="https://cdn-icons-png.flaticon.com/512/763/763965.png" card-title="Home"/>
+    <Card link="#/about" image="https://cdn-icons-png.flaticon.com/512/763/763965.png" card-title="About"/>
+    <Card link="#/non-existent-path" image="https://cdn-icons-png.flaticon.com/512/763/763965.png" card-title="Broken Link"/>
+  </section>
 </template>
 
 <style scoped>
-
+.cards-container {
+  display: grid;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+  gap: 3rem;
+}
 </style>
---
+++ src/shared/ui/Card.vue
@@ -0,0 +1,57 @@
+<script setup>
+  defineProps({
+    link: String,
+    image: String,
+    cardTitle: String
+  })
+</script>
+
+<template>
+  <a :href="link" class="category-card">
+    <img :src="image" alt="" class="category-card-image">
+    <h2>{{ cardTitle }}</h2>
+  </a>
+</template>
+
+<style scoped>
+.category-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  width: 18rem;
+  height: 18rem;
+  border-radius: .75rem;
+  padding: 1rem;
+  background-color: #222327;
+  cursor: pointer;
+  transition: all .2s;
+}
+
+.category-card:hover {
+  transform: scale(1.04);
+}
+
+.category-card-image {
+  width: 8rem;
+  height: 8rem;
+}
+
+@media only screen and (max-width: 810px) {
+  .category-card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    width: 18rem;
+    height: 18rem;
+    border-radius: .75rem;
+    padding: 1rem;
+    background-color: red;
+    cursor: pointer;
+    transition: all .2s;
+  }
+}
+
+</style>
