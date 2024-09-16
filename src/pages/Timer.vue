d777bc6cdf05e18e3f5bc19f449be3acced9c544
SrVleh <srvleh@gmail.com>
2024-09-16 12:06:39 +0200

    Basic timer form functionality

--- src/pages/Timer.vue
+++ src/pages/Timer.vue
@@ -5,42 +5,68 @@
     AMRAP: "AMRAP",
     FT: "For Time",
     EMOM: "EMOM",
-    TBT: "Tabata"
+    TBT: "Tabata",
+    RST: "Rest"
   })
-
-  let sessionData = reactive({
+  let newSessionData = reactive({
     type: "",
-    minutes: 1,
-    loops: 0
+    for: 1,
+    every: 1
   })
+  let isNewSession = ref(false);
+  let sessionsList = reactive([])
+
+  function addSessionToList() {
+    let sessionData = { ...newSessionData }
+    sessionsList.push(sessionData)
+    resetFormData()
+  }
+
+  function resetFormData() {
+    newSessionData.type = "";
+    newSessionData.for = 1;
+    newSessionData.every = 1;
+  }
+
 </script>
 
 <template>
   <h1>Timer</h1>
-  <p v-if="sessionData">{{ sessionData }}</p>
-  <p>{{ sessionData.type }}</p>
-  <p>{{ sessionData.minutes }}</p>
-  <p>{{ sessionData.loops }}</p>
+
+  <div v-for="session in sessionsList" style="width: 100%; border: 1px solid white; border-radius: 8px; display:flex; flex-direction: column; justify-content: center; align-items: center;">
+    <h3>{{ session.type }}</h3>
+    <p v-if="session.type === trainingTypes.EMOM">Every: {{ session.every }} minutes</p>
+    <p>For: {{ session.for }} minutes</p>
+  </div>
+
   <form>
-    <fieldset>
-      <label for="types">Choose training type:</label>
-      <select name="types" id="types" v-model="sessionData.type">
-        <option :value="trainingTypes.AMRAP">{{ trainingTypes.AMRAP }}</option>
-        <option :value="trainingTypes.FT">{{ trainingTypes.FT }}</option>
-        <option :value="trainingTypes.EMOM">{{ trainingTypes.EMOM }}</option>
-        <option :value="trainingTypes.TBT">{{ trainingTypes.TBT }}</option>
-      </select>
+    <button type="button" v-on:click="isNewSession = !isNewSession" v-if="!isNewSession">Add new training</button>
+
+    <fieldset v-if="isNewSession">
+      <fieldset>
+        <label for="types">Choose training type:</label>
+        <select name="types" id="types" v-model="newSessionData.type">
+          <option :value="trainingTypes.AMRAP">{{ trainingTypes.AMRAP }}</option>
+          <option :value="trainingTypes.FT">{{ trainingTypes.FT }}</option>
+          <option :value="trainingTypes.EMOM">{{ trainingTypes.EMOM }}</option>
+          <option :value="trainingTypes.TBT">{{ trainingTypes.TBT }}</option>
+          <option :value="trainingTypes.RST">{{ trainingTypes.RST }}</option>
+        </select>
+      </fieldset>
+
+      <fieldset>
+        <label for="minutes">During minutes:</label>
+        <input type="number" min="1" value="1" v-model="newSessionData.for" id="minutes" placeholder="1">
+      </fieldset>
+
+      <fieldset v-if="newSessionData.type === trainingTypes.EMOM">
+        <label for="loops">Every minutes</label>
+        <input type="number" min="1" id="loops" v-model="newSessionData.every">
+      </fieldset>
     </fieldset>
 
-    <fieldset>
-      <label for="minutes">Minutes</label>
-      <input type="number" min="1" value="1" v-model="sessionData.minutes" id="minutes" placeholder="1">
-    </fieldset>
-
-    <fieldset v-if="sessionData.type == trainingTypes.EMOM">
-      <label for="loops">Loops</label>
-      <input type="number" min="1" id="loops" v-model="sessionData.loops">
-    </fieldset>
+    <button type="button" v-if="isNewSession" v-on:click="addSessionToList">Add to session</button>
+    <button type="button">Start training!</button>
   </form>
 </template>
 
